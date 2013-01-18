c0dbb83bfdcd3fbce6332ec05430cd836da4c0c7
hkammada <hari.hopes@gmail.com>
2013-01-18 05:00:03 -0800

    This is the read me file for Myscript1.
    This is the read me file to know how to use the myscript1 .

---
+++ READ me for Myscript.sh
@@ -0,0 +1,9 @@
+This file explains the steps to use "Myscript1.sh" . 
+
+--1 . You should have root privilege to execute this script .
+--2 . Its self explanatory script, the echoes will let you know how to respond and what is it actually doing . 
+--3 . Please cheange the permission of the script using 
+
+chmod +x myscript1.sh 
+
+then run the script as usual shell script . 
